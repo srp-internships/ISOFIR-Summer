087dc06b7b6ffc2807adb47ae2abb0786d47793f
firuz2006 <firuzisoboev@gmail.com>
2023-06-21 10:31:18 +0500

    testing Initial

---
+++ Testing/TestNinja/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,21 @@
+﻿namespace TestNinja.Fundamentals;
+
+public class ErrorLogger
+{
+    public string LastError { get; set; }
+
+    public event EventHandler<Guid> ErrorLogged;
+
+    public void Log(string error)
+    {
+        if (string.IsNullOrWhiteSpace(error))
+            throw new ArgumentNullException();
+
+        LastError = error;
+
+        // Write the log to a storage
+        // ...
+
+        ErrorLogged?.Invoke(this, Guid.NewGuid());
+    }
+}
