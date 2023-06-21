25f6bcc9005a2d3d20d249c611537d6105d83c28
firuz2006 <firuzisoboev@gmail.com>
2023-06-21 12:35:24 +0500

    FizzBuzzTests released

--- Testing/TestNinja.Tests/FizzBuzzTests.cs
+++ Testing/TestNinja.Tests/FizzBuzzTests.cs
@@ -1,10 +1,48 @@
 ﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.Tests;
 
 [TestFixture]
 public class FizzBuzzTests
 {
-    // [SetUp]
-    // public void GetOutput_
+    [Test]
+    [TestCase(3)]
+    [TestCase(33)]
+    [TestCase(333)]
+    public void GetOutput_InputIsDivisibleOnly3(int input)
+    {
+        var result = FizzBuzz.GetOutput(input);
+        
+        Assert.That(result,Is.EqualTo("Fizz"));
+    }
+    
+    [Test]
+    [TestCase(5)]
+    [TestCase(10)]
+    [TestCase(110)]
+    public void GetOutput_InputIsDivisibleOnly5(int input)
+    {
+        var result = FizzBuzz.GetOutput(input);
+        
+        Assert.That(result,Is.EqualTo("Buzz"));
+    }
+    
+    [Test]
+    [TestCase(15)]
+    public void GetOutput_InputIsDivisible5And3(int input)
+    {
+        var result = FizzBuzz.GetOutput(input);
+        
+        Assert.That(result,Is.EqualTo("FizzBuzz"));
+    }
+    
+    [Test]
+    [TestCase(1)]
+    public void GetOutput_InputIsDivisible5Or3_ReturnTheSameNumber(int input)
+    {
+        var result = FizzBuzz.GetOutput(input);
+        
+        Assert.That(result,Is.EqualTo(input.ToString()));
+    }
 }
