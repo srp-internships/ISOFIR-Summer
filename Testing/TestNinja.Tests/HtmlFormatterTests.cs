e5630743bc154f1a4d82237b2a81b147328340cb
firuz2006 <firuzisoboev@gmail.com>
2023-06-21 14:37:10 +0500

    HtmlFormatterTests released

---
+++ Testing/TestNinja.Tests/HtmlFormatterTests.cs
@@ -0,0 +1,25 @@
+﻿using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests;
+
+[TestFixture]
+public class HtmlFormatterTests
+{
+    private HtmlFormatter _htmlFormatter=null!;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _htmlFormatter = new HtmlFormatter();
+    }
+    
+    [Test]
+    public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+    {
+        var result = _htmlFormatter.FormatAsBold("string");
+        
+        Assert.That(result,Does.StartWith("<strong>").IgnoreCase);
+        Assert.That(result,Does.EndWith("</strong>").IgnoreCase);
+        Assert.That(result,Does.Contain("string").IgnoreCase);
+    }
+}
