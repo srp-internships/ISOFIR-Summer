e64aec6481e849fd232e9c60708a74b59aba9468
firuz2006 <firuzisoboev@gmail.com>
2023-06-21 11:42:13 +0500

    TestNinja.Tests ReservationTests released

---
+++ Testing/TestNinja.Tests/FizzBuzzTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace TestNinja.Tests;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+    // [SetUp]
+    // public void GetOutput_
+}
---
+++ Testing/TestNinja.Tests/ReservationTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests;
+
+[TestFixture]
+public class ReservationTests
+{
+    private Reservation _reservation=null!;
+    private User _user=null!;
+    [SetUp]
+    public void SetUp()
+    {
+        _user = new User();
+        _reservation = new Reservation();
+    }
+    
+    [Test]
+    public void CanBeCancelledBy_AdminCancelling_ReturnTrue()
+    {
+        _user.IsAdmin = true;
+        
+        var result = _reservation.CanBeCancelledBy(_user);
+        
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_SameUserCancelling_ReturnTrue()
+    {
+        _reservation.MadeBy = _user;
+        
+        var result = _reservation.CanBeCancelledBy(_user);
+        
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_AnotherUserCancelling_ReturnTrue()
+    {
+        var result = _reservation.CanBeCancelledBy(_user);
+        
+        Assert.That(result, Is.False);
+    }
+}
