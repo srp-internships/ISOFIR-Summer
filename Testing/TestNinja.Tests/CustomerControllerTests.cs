636cecf4bf030c9dd3d0df66e4192586e0301f46
firuz2006 <firuzisoboev@gmail.com>
2023-06-22 13:26:02 +0500

    CustomerControllerTests.cs released

---
+++ Testing/TestNinja.Tests/CustomerControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests;
+
+[TestFixture]
+public class CustomerControllerTests
+{
+    private CustomerController _customerController=null!;
+    [SetUp]
+    public void Setup()
+    {
+        _customerController = new CustomerController();
+    }
+    [Test]
+    public void GetCustomer_WhenIdEqual0_ReturnNotFoundResult()
+    {
+        var result = _customerController.GetCustomer(0);
+        
+        Assert.That(result, Is.TypeOf<NotFound>());
+    }
+
+    [Test]
+    public void GetCustomer_IdIsGreaterThan0_ReturnOkResult()
+    {
+        var result = _customerController.GetCustomer(62);
+        
+        Assert.That(result,Is.TypeOf<Ok>());
+    }
+}
