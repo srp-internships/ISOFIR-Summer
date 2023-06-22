1c9659f4fdfd81b01f5c7fce0236e31099fa8f34
firuz2006 <firuzisoboev@gmail.com>
2023-06-22 14:25:43 +0500

    ProductTests.cs released

---
+++ Testing/TestNinja.Tests/MockingTests/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿using TestNinja.Mocking;
+
+namespace TestNinja.Tests.MockingTests;
+
+[TestFixture]
+public class ProductTests
+{
+    private Product _product = null!;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _product = new Product();
+    }
+
+    [Test]
+    public void GetPrice_WhenCustomerIsGold_Discount70Percent()
+    {
+        _product.ListPrice = 10;
+        
+        var price = _product.GetPrice(new Customer { IsGold = true });
+        
+        Assert.That(price,Is.EqualTo(7));
+    }
+
+    [Test]
+    public void Get_WhenCustomerIsCommon_ReturnListPrice()
+    {
+        _product.ListPrice = 100;
+
+        var price = _product.GetPrice(new Customer());
+        
+        Assert.That(price,Is.EqualTo(100));
+    }
+}
