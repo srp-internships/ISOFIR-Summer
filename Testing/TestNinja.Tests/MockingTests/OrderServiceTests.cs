0b879e5dfbaa1a16aeb58202a6e062aef51f8444
firuz2006 <firuzisoboev@gmail.com>
2023-06-22 14:33:21 +0500

    Moq added && OrderServiceTests.cs released

---
+++ Testing/TestNinja.Tests/MockingTests/OrderServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using TestNinja.Mocking;
+
+namespace TestNinja.Tests.MockingTests;
+
+[TestFixture]
+public class OrderServiceTests
+{
+    private OrderService _orderService=null!;
+    private Mock<IStorage> _storageMock=null!;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _storageMock = new Mock<IStorage>();
+        _orderService = new OrderService(_storageMock.Object);
+    }
+
+    [Test]
+    public void PlaceOrder_WhenCalled_StoreTheOrder()
+    {
+        var order = new Order();
+
+        _orderService.PlaceOrder(order);
+        
+        _storageMock.Verify(s=>s.Store(order));
+    }
+}
