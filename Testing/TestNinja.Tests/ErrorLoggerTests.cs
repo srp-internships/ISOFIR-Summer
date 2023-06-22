af266be78a94641b16e14048f37cb238b39dc65c
firuz2006 <firuzisoboev@gmail.com>
2023-06-22 13:44:58 +0500

    ErrorLoggerTests.cs released

---
+++ Testing/TestNinja.Tests/ErrorLoggerTests.cs
@@ -0,0 +1,44 @@
+﻿using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests;
+
+[TestFixture]
+public class ErrorLoggerTests
+{
+    private ErrorLogger _errorLogger = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _errorLogger = new ErrorLogger();
+    }
+
+    [Test]
+    public void Log_WhenCalled_FillLastErrorProperty()
+    {
+        _errorLogger.Log("this is a last error");
+
+        Assert.That(_errorLogger.LastError, Is.EqualTo("this is a last error"));
+    }
+
+    [Test]
+    [TestCase("     ")]
+    [TestCase("")]
+    [TestCase(" ")]
+    [TestCase(null)]
+    public void Log_MessageIsNullOrWhiteSpace(string message)
+    {
+        Assert.That(()=>_errorLogger.Log(message),Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Log_ValidMessage_InvokeErrorLoggedEvent()
+    {
+        var id = Guid.Empty;
+
+        _errorLogger.ErrorLogged += (_, argId) => id = argId;
+        _errorLogger.Log("the event test log");
+        
+        Assert.That(id,Is.Not.EqualTo(Guid.Empty));
+    }
+}
